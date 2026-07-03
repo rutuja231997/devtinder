22e304e91882e9e9f00767570738452749f2be79
Rutuja <krutuja311@gmail.com>
2026-07-03 13:38:13 +0530

    feat: fix the app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -32,8 +32,10 @@ app.use(cookieParser());
 connectDB()
   .then(() => {
     console.log("database connection established");
-    app.listen(() => {
-      console.log(`server is listening on ${process.env.BACKEND_URL}`);
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `server is listening on http://localhost:${process.env.PORT}`,
+      );
     });
   })
   .catch((err) => {
